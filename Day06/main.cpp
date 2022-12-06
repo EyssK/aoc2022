2c1d147869f6483aecb584ac2f177f8732f3d79d
Kevin Eyssartier <kevin.eyssartier@thalesgroup.com>
2022-12-06 11:42:30 +0100

    Day06 template

--- Day06/main.cpp
+++ Day06/main.cpp
@@ -2,33 +2,18 @@
 #include <fstream>
 #include <string>
 #include <set>
-//#include <regex>
-//#include <algorithm>
 
+template <int DEPTH>
 int get_marker_pos(std::string input) {
     for (int i = 0; i<input.size(); i++) {
-        std::set<char> marker {input[i],input[i+1],input[i+2],input[i+3]};
-        if (marker.size() == 4) {
-            for(auto e : marker)
-                std::cout << e << std::endl;
-            return i+4;
+        std::set<char> marker;
+        for (int d = 0; d<DEPTH; d++) {
+            marker.insert(input[i+d]);
         }
-    }
-    return 0;
-}
-
-int get_message_pos(std::string input) {
-    for (int i = 0; i<input.size(); i++) {
-        std::set<char> marker {
-            input[i],input[i+1],input[i+2],input[i+3],
-            input[i+4],input[i+5],input[i+6],input[i+7],
-            input[i+8],input[i+9],input[i+10],input[i+11],
-            input[i+12],input[i+13]
-        };
-        if (marker.size() == 14) {
+        if (marker.size() == DEPTH) {
             for(auto e : marker)
                 std::cout << e << std::endl;
-            return i+14;
+            return i+DEPTH;
         }
     }
     return 0;
@@ -41,7 +26,7 @@ auto main(int argc, char** argv) -> int {
         std::string line;
 
         if(std::getline(file, line)) {
-            std::cout << get_marker_pos(line) <<  std::endl;
+            std::cout << get_marker_pos<4>(line) <<  std::endl;
         }
     }
     {
@@ -50,7 +35,7 @@ auto main(int argc, char** argv) -> int {
         std::string line;
 
         if(std::getline(file, line)) {
-            std::cout << get_message_pos(line) <<  std::endl;
+            std::cout << get_marker_pos<14>(line) <<  std::endl;
         }
     }
     return 0;
